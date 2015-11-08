9d626e7e7965b8ea3098dff8fc638b74b3b8f10e
dluo11 <dluo@caltech.edu>
2015-11-08 06:39:07 -0800

    Tried to write events for host
    Not quite sure exactly how to call things, tried to look at events from
    other files, but not many examples.

--- network_simulator/host.py
+++ network_simulator/host.py
@@ -1,6 +1,7 @@
 """This module contains all Host definition.
 """
 
+import inspect
 from device import *
 
 
@@ -23,4 +24,54 @@ class Host(Device):
         self.flow_packets_received = flow_packets_received
         self.link = link
 
-    # TODO(team): More event subclasses to come?
+
+class HostReceivedPacketEvent(Event):
+    """
+    Host receives packets from a flow.
+    """
+
+    def run(self, main_event_loop, statistics):
+        """
+        Sanity check: make sure the destination was this Host,
+        and that this isn’t a RouterPacket. Update statistics.
+
+        :param MainEventLoop main_event_loop: event loop where new Events will
+        be scheduled.
+        :param Statistics statistics: the Statistics to update
+        """
+        if inspect.isclass(self.flow_packets_received) == RouterPacket:
+            raise ValueError ("Packet received is a router packet.")
+        if self.flow_packets_received.dest_addr != self.address:
+            raise ValueError ("Destination was not this host.")
+        else:
+            #check statistics?
+            pass
+
+    def schedule_new_events(self, main_event_loop, statistics):
+        """
+        If the packet was an ACK, look up the corresponding Flow and let it
+        know that its packet was received.
+        Schedule a FlowReceivedAckEvent immediately.
+        Populate the AckPacket for selective repeat later.
+
+        If the packet was a data packet, generate a corresponding ACK and
+        schedule it via a DeviceToLink event unless that packet was already
+        received earlier by this Host.
+
+        For AckPackets, need to set original data packet’s start time so we can
+        calculate RTT later.
+        Also AckPacket’s ID should be the original DataPacket’s ID.
+
+        :param MainEventLoop main_event_loop: event loop where new Events will
+        be scheduled.
+        :param Statistics statistics: the Statistics to update
+        """
+        if inspect.isclass(self.flow_packets_received) == AckPacket:
+            event.schedule_new_events(self, main_event_loop)
+            flow.handle_packet_success(self, packet_id)
+        if inspect.isclass(self.flow_packets_received) == DataPacket and \
+                        packet_id not in self.flow_packets_received:
+            event.schedule_new_events(self, DeviceToLink)
+        else:
+            #check statistics?
+            pass
